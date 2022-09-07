2c033f897dac419af94f58faafcab5fbbeefad9d
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-09-07 19:27:33 +0900

    feat/ 게시글 수정, 삭제 기능 구현

--- post/services/post_service.py
+++ post/services/post_service.py
@@ -1,18 +1,50 @@
 from typing import Dict
-from post.serializers import PostSerializer
+from post.serializers import CreatePostSerializer, PostSerializer
+from post.models import Post as PostModel
 
 
 def create_post(create_post_data: Dict[str, str]) -> None:
     """
-    Post의 Create를 담당하는 Service
+    게시글 생성을 담당하는 Service
     Args :
         create_post_data (dict) : {
-            'title' (str): 게시글의 제목,
-            'content' (str) : 게시글의 내용
+            title (str): 게시글의 제목,
+            content (str) : 게시글의 내용
         }
     Return :
         None
     """
-    post_serializer = PostSerializer(data=create_post_data)
+    post_serializer = CreatePostSerializer(data=create_post_data)
     post_serializer.is_valid(raise_exception=True)
     post_serializer.save()
+    
+    
+def update_post(post_id: int, update_post_data: Dict[str, str]) -> Dict[str, str]:
+    """
+    게시글 수정을 담당하는 Service
+    Args :
+        post_id (int): posts.Post 외래키, url에 담아서 보내줌,
+        update_post_data (dict): {
+            title (str): 게시글의 제목 or
+            content (str): 게시글의 내용
+        }
+    Return :
+        dict[str, str]
+    """
+    update_post = PostModel.objects.get(id=post_id)
+    update_post_serializer = PostSerializer(update_post, update_post_data, partial=True)
+    update_post_serializer.is_valid(raise_exception=True)
+    update_post_serializer.save()
+    return ({"update_post": update_post_serializer.data})
+
+
+def delete_post(post_id: int) -> None:
+    """
+    게시글 삭제를 담당하는 Service
+    Args :
+        post_id (int): posts.Post 외래키, url에 담아서 보내줌
+    Return :
+        None
+    """
+    delete_post = PostModel.objects.get(id=post_id)
+    delete_post.delete()
--- post/views.py
+++ post/views.py
@@ -1,15 +1,21 @@
+from django.contrib.auth.hashers import check_password
 from rest_framework import status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from post.services.post_service import (
     create_post,
+    update_post,
+    delete_post
 )
+from post.models import Post as PostModel
+
 
 class PostView(APIView):
     """
     게시글의 CRUD를 담당하는 View
     """
+
     def post(self, request):
         try:
             if request.data:
@@ -19,3 +25,29 @@ class PostView(APIView):
             name = list(e.detail.keys())[0]
             error = ''.join([str(value) for values in e.detail.values() for value in values])
             return Response({name: error}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, post_id):
+        try:
+            post = PostModel.objects.get(id=post_id)
+        except PostModel.DoesNotExist:
+            return Response({'detail': '수정할 게시글이 존재하지 않습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        if check_password(request.data.pop('password'), post.password):
+            if request.data['content'] == {}:
+                return Response({'detail': '수정할 내용이 비어있습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+            if post.content == request.data['content']: 
+                return Response({'detail': '수정할 내용을 입력해주세요.'}, status=status.HTTP_400_BAD_REQUEST) 
+            updated_log = update_post(post_id, request.data)
+            return Response(updated_log, status=status.HTTP_201_CREATED)
+        return Response({'detail': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+    
+    def delete(self, request, post_id):
+        try:
+            post = PostModel.objects.get(id=post_id)    
+        except PostModel.DoesNotExist:
+            return Response({'detail': '삭제할 게시글이 존재하지 않습니다.'}, status=status.HTTP_404_NOT_FOUND)
+        if check_password(request.data.pop('password'), post.password):
+            delete_post(post_id)
+            return Response({'detail': '게시글이 삭제되었습니다.'}, status=status.HTTP_200_OK)
+        return Response({'detail': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
